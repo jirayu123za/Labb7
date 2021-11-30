679084ac79d81ac7fca1b861f8ec32a4f96b2bf5
jirayu123za <jirayu004za@gmail.com>
2021-11-30 15:20:35 +0700

    Comp

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -1,41 +1,70 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <cstring>
 
 using namespace std;
 
-string func1(string x){
+string func1(string x)
+{
 	int i = 0, L = x.size();
 	string y = "";
-	while(i < L){
-		y += x[L-i-1];
+	while (i < L)
+	{
+		y += x[L - i - 1];
 		i++;
 	}
 	return y;
 }
 
-string func2(string x){
+string func2(string x)
+{
 	int i = 0, L = x.size();
 	string y = "";
-	while(i < L){
+	while (i < L)
+	{
 		y += toupper(x[i]);
 		i++;
 	}
-	return y;	
+	return y;
 }
 
-string func3(string x){
+string func3(string x)
+{
 	int i = 0, L = x.size();
 	string y = "";
-	while(i < L){
+	while (i < L)
+	{
 		y += tolower(x[i]);
 		i++;
 	}
-	return y;	
+	return y;
 }
 
-int main(){
-    cout >> "Input text: "
-    cout >> "Reversed text: "
-    cout >> "Palindrome: "
-    return 0;
+int main()
+{
+	char string1[20];
+	int i, length;
+	int a = 0;
+
+	cout << "Input text: ";
+	cin >> string1;
+	cout << "Reversed text: " << func1(string1) << "\n";
+	string string2 = func3(string1);
+
+	length = strlen(string1);
+
+	for (i = 0; i < length; i++)
+	{
+		if (string2[i] != string2[length - i - 1])
+			a = 1;
+		break;
+	}
+
+	if (a)
+	{
+		cout << "Palindrome: No" << "\n";
+	
+	}else{
+		cout << "Palindrome: Yes" << "\n";
+	}
+	return 0;
 }
