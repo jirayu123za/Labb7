b8826dfb6e0badf21533fc6abf44917768f336d7
jirayu123za <jirayu004za@gmail.com>
2021-11-30 15:53:25 +0700

    hard

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,55 @@
-#include<iostream>
+#include <iostream>
+#include <cmath>
 
 using namespace std;
 
+int adiff(int A, int B)
+{
+  int C;
+  if (A - B >= 0)
+  {
+    C = A - B;
+    if (C % 360 == 0)
+    {
+      C = 0;
+      return C;
+    }
+    else if (C > 180)
+    {
+      return abs(C - 360);
+    }
+    else
+    {
+      return C;
+    }
+  }
+  else
+  {
+    C = B - A;
+    if (C % 360 == 0)
+    {
+      C = 0;
+      return C;
+    }
+    else if (C > 180)
+    {
+      return abs(C - 360);
+    }
+    else
+    {
+      return C;
+    }
+  }
+}
 
-int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+int main()
+{
+  cout << adiff(180, 270) << "\n";
+  cout << adiff(210, 45) << "\n";
+  cout << adiff(0, 360) << "\n";
+  cout << adiff(10, 350) << "\n";
+  cout << adiff(95, 260) << "\n";
+  cout << adiff(90, -90) << "\n";
+  cout << adiff(1000, 280) << "\n";
+  cout << adiff(60, 244) << "\n";
 }
