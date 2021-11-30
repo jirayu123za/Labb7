f0732bb7ea01ad2b9aaa6b4be3457acaf1adba9b
jirayu123za <jirayu004za@gmail.com>
2021-11-30 16:43:44 +0700

    Fix_all

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,37 +1,41 @@
 #include <iostream>
 
 using namespace std;
+typedef long long int lli;
 
 int main()
 {
 
-	int i = 0, P[5000], k, result;
+	lli i = 0;
+	lli P[5000];
+
 	while (i < 5000)
 	{
 		P[i] = 2 * i + 1;
 		i++;
 	}
 
+	lli k;
 	cout << "Please input k: ";
 	cin >> k;
 
-	if (k <= 0)
+	lli result;
+	if (k > 0)
 	{
-		cout << "Invalid input!!!";
-	}
-	else
-	{
-		for (i = 0; i < 5000; i++)
-		{
-			if (P[i] % k == 0)
-			{
+		i = 0;
+		result = 0;
+		while(i < 5000){
+			if(P[i]%k == 0){
 				result = result - P[i];
 			}else{
 				result = result + P[i];
 			}
-			
+			i = i+1;
 		}
+		cout << "Result = " << result << endl;
+	}else{
+		cout << "Invalid input!!!" << endl;
 	}
-
+	
 	return 0;
 }
